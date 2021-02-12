07c56c3c3e3c2ad26214567b8f542f733b518a34
FIC <FIC@info.local>
2021-02-12 18:14:54 -0200

    Projeto de cadastro finalizado

--- Controllers/CadastroController.cs
+++ Controllers/CadastroController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Instadev.Controllers
 {
+    [Route("Cadastro")]
     public class CadastroController : Controller
     {
         public IActionResult Index()
